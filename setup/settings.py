89f43e84431d6768792b224e0fc948e2de9b9dd0
Sergio Vinícius <74887082+Sergio1102@users.noreply.github.com>
2025-10-21 19:26:57 -0300

    montagem do sistema para primeiros testes

--- setup/settings.py
+++ setup/settings.py
@@ -117,7 +117,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/accounts/login/'
